6f4c6dd6264219b94528fe3a758ca2a0cefa9525
Nyula <nabilaalhassan70@gmail.com>
2023-11-20 15:40:27 +0000

    lines 85, 104 and 112 updated

--- main.tf
+++ main.tf
@@ -12,17 +12,18 @@ provider "aws" {
 }
 
 # Create a VPC
-resource "aws_vpc" "eco-css_vpc" {
+resource "aws_vpc" "css_vpc" {
   cidr_block = "10.0.0.0/18" 
 
   tags = {
-    Name = "Eco-car-sharing service"
+    Name = "Eco-car-sharing-service"
   }
 }
 
+
 # Create  public and  private subnets within the VPC
-resource "aws_subnet" "public_css1" {
-  vpc_id                  = aws_vpc.eco-css_vpc.id
+resource "aws_subnet" "public-css1" {
+  vpc_id                  = aws_vpc.css_vpc.id
   cidr_block              = "10.0.1.0/24" 
   availability_zone       = "us-east-1a" 
 
@@ -31,8 +32,8 @@ resource "aws_subnet" "public_css1" {
   }
 }
 
-resource "aws_subnet" "private_css1" {
-  vpc_id                  = aws_vpc.eco-css_vpc.id
+resource "aws_subnet" "private-css1" {
+  vpc_id                  = aws_vpc.css_vpc.id
   cidr_block              = "10.0.2.0/24" 
   availability_zone       = "us-east-1b" 
 
@@ -41,13 +42,13 @@ resource "aws_subnet" "private_css1" {
   }
 }
 
-# Launch four (2) EC2 instance
+# Launch two (2) EC2 instance
 
 resource "aws_instance" "public_1" {
   ami               = "ami-0e8a34246278c21e4" 
   instance_type     = "t2.micro"              
-  key_name          = "css_key"    
-  subnet_id         = aws_subnet.public_css1.id
+  key_name          = "my-private-key"    
+  subnet_id         = aws_subnet.public-css1.id
   associate_public_ip_address = true
 
 
@@ -56,11 +57,11 @@ resource "aws_instance" "public_1" {
   }
 }
 
-resource "aws_instance" "private_1" {
+resource "aws_instance" "private-1" {
   ami               = "ami-0e8a34246278c21e4" 
   instance_type     = "t2.micro"              
-  key_name          = "css_key"    
-  subnet_id         = aws_subnet.private_css1.id
+  key_name          = "my-private-key"    
+  subnet_id         = aws_subnet.private-css1.id
   
  
   tags = {
@@ -68,62 +69,62 @@ resource "aws_instance" "private_1" {
   }
 }
 
-resource "aws_launch_configuration" "css_lc" {
+
+#ASG launch configuration is used to configure the EC2 instances that will be launched by the ASG
+resource "aws_launch_configuration" "css-launch-config" {
   name = "css-launch-config"
  image_id = "ami-0e8a34246278c21e4"  
   instance_type = "t2.micro"
-  
+  user_data    = <<-EOF
+                    #!/bin/bash
+                    echo "Hello, World!" > index.html
+                    EOF
 }
 
 # Define Auto scaling group
-resource "aws_autoscaling_group" "css_asg" {
-  launch_configuration = aws_launch_configuration.css_asg.id
+resource "aws_autoscaling_group" "css-asg"{
+  launch_configuration = aws_launch_configuration.css-launch-config.id
   min_size             = 1
   max_size             = 5
   desired_capacity     = 2
+  vpc_zone_identifier = ["public-css1" , "private-css1"]
+  
+  health_check_type          = "EC2"
+  health_check_grace_period  = 200
+  force_delete               = true
 
+  tag {
+    key                 = "Name"
+    value               = "web-instance"
+    propagate_at_launch = true
+  }
 }
 
 # Define Target Group
-resource "aws_lb_target_group" "css_tg" {
+resource "aws_lb_target_group" "css-tg" {
   name        = "css-tg"
   port        = 80  
   protocol    = "HTTP" 
-  vpc_id      = "aws_vpc.eco-css_vpc.id"  
+  vpc_id      = aws_vpc.css_vpc.id
 }
 
 # Define Load Balancer
-resource "aws_lb" "css_alb" {
-  name               = "car-sharing"
+resource "aws_lb" "css-alb" {
+  name               = "css-alb"
   internal           = false  
   load_balancer_type = "application"  
-  subnets            = ["aws_subnet.public_css1.id", "aws_subnet.private_css1.id"]  
+  subnets            = ["public-css1" , "private-css1"]
 }
 
 # Define Listener
-resource "aws_lb_listener" "css_listener" {
-  load_balancer_arn = aws_lb.css_alb.arn
+resource "aws_lb_listener" "css-listener" {
+  load_balancer_arn = aws_lb.css-alb.arn
   port              = 80  
   protocol          = "HTTP"  
 
   default_action {
-    target_group_arn = aws_lb_target_group.css_tg.arn  
+    target_group_arn = aws_lb_target_group.css-tg.arn  
     type             = "forward"
-  }
-}
-
-# Define Routing Rules 
-  resource "aws_lb_listener_rule" "css_listener_rule"{
-
-  listener_arn = aws_lb_listener.css_listener.arn
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.css_tg.arn 
-  }
-  condition {
-    path_pattern {
-      values = "/"
-    }
   }
 }
 
